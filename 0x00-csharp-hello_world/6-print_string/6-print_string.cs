99271309dd06827df839693beead70855f73ab49
calvert1212 <calvert1212@gmail.com>
2022-08-29 21:25:56 -0500

    Create 6-print_string.cs

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.WriteLine("{0}{0}{0}\n{0:9}", str, str, str, str);
+        }
+}
