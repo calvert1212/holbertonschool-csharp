693f064afcbc0028a837c6bc82b6996dcdd2b9ce
calvert1212 <calvert1212@gmail.com>
2022-08-30 13:11:38 -0500

    Create 3-print_alphabt.cs

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char alph = 'a'; alph <= 'z'; alph++)
+        {
+        if (alph != 'e' && alph != 'q')
+            Console.Write(loop);
+        }
+    }
+}
