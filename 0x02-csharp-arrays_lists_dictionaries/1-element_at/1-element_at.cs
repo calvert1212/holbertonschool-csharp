522575a9874e74b25a5f772320e788bc4b131024
calvert1212 <calvert1212@gmail.com>
2022-08-31 22:59:30 +0000

    Adding 1

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Array
+{
+    public static int elementAt(int[] arr, int x)
+    {
+        if (x < 0 || x >= arr.Length)
+        {
+            Console.WriteLine("Index out of range");
+            return -1;
+        }
+        return arr[x];
+    }
+}
