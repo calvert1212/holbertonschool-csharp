4bfe8e0f7ebae9d12027404c5b9da16f57889a7a
calvert1212 <calvert1212@gmail.com>
2022-08-30 20:47:03 -0500

    Create 5-print_comb.cs

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            if (i != 99)
+            {
+                Console.Write("{0:D2}, ", i);
+            }
+            else
+            {
+                Console.Write("{0:D2}\n", i);
+            }
+        }
+    }
