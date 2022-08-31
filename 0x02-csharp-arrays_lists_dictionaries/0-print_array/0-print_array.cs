c7060e079bdb5515578426da87fac886157ec2e4
calvert1212 <calvert1212@gmail.com>
2022-08-31 22:42:13 +0000

    Testing first files

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] newArray = new int[size];
+
+        for (int loop = 0; loop < size; loop++)
+        {
+            newArray[loop] = loop;
+            Console.Write(loop);
+            if (loop != size - 1)
+            {
+                Console.Write(" ");
+            }
+        }
+
+        Console.WriteLine();
+        return newArray;
+    }
+}
