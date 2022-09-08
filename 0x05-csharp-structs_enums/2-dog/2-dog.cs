4469b1f13584476156e327f34bab10bbb5fb72b7
calvert1212 <calvert1212@gmail.com>
2022-09-08 01:02:59 +0000

    Adding work so far

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog
+{
+    public Dog(string pName, float pAge, string pOwner, Rating pRating)
+    {
+        name = pName;
+        age = pAge;
+        owner = pOwner;
+        rating = pRating;
+    }
+
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
