85cabb71fda01c69781da7b5f1e68daeb982dbc5
calvert1212 <calvert1212@gmail.com>
2022-09-22 18:40:35 +0000

    Task one

---
+++ 0x03-csharp-delegates_events/0-universal_health/Player.cs
@@ -0,0 +1,21 @@
+using System;
+/// <summary>Class for a player.</summary>
+public class Player{
+	private string name;
+	private float maxHp;
+	private float hp;
+	/// <summary>Sets name and hp.</summary>
+	public Player(string name="Player", float maxHp=100f){
+		this.name = name;
+		if (maxHp <= 0){
+			maxHp = 100f;
+			Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+		}
+		this.maxHp = maxHp;
+		this.hp = maxHp;
+	}
+	/// <summary>Displays current health to the console.</summary>
+	public void PrintHealth(){
+		Console.WriteLine($"{name} has {hp} / {maxHp} health");
+	}
+}
---
+++ 0x03-csharp-delegates_events/README.md
@@ -0,0 +1,1 @@
+Learning about delegates and events!
